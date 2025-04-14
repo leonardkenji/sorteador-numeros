a2735e4062c101080126a86caa7677d7a0929a15
Leonardo Kenji <leonardkenji@gmail.com>
2025-04-14 13:44:12 +0900

    Aula de Logica de Programacao pt1

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let quantidadeDeNumeros = document.getElementById("quantidade").value;
+    let de = document.getElementById("de").value;
+    let ate = document.getElementById("ate").value;
+
+    alert(`voce pediu ${quantidadeDeNumeros} numeros`);
+    alert(`entre ${de}`);
+    alert(`e ${ate} numeros`);
+}
